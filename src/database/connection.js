169e778a2a5986f0aa35e2e5ff8f9d0d18d133b0
João Victor Cavalcante <101560833+jotacoding@users.noreply.github.com>
2022-05-09 23:50:58 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 
 export const connection = new Sequelize(
-    process.env.DATABASE_URL,
+    'postgres://filmes_bp20_user:Gw4nWCtdtSnvRORUmwfrqC3ocEZG77RO@dpg-c9sriarru51ph66f7an0-a.oregon-postgres.render.com/filmes_bp20',
     
    {
         dialect: 'postgres',
